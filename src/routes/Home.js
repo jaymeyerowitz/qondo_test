4da7bbaa595b5d9901b1b8e72e4fb84cbe48bce0
Jay Meyerowitz <jaymeyerowitz@gmail.com>
2021-01-20 18:43:20 +0200

    Add complete page and small style changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,6 +5,9 @@ import TwitterIcon from "@material-ui/icons/Twitter"
 import SentimentVerySatisfiedIcon from "@material-ui/icons/SentimentVerySatisfied"
 import SentimentSatisfiedIcon from "@material-ui/icons/SentimentSatisfied"
 import MoodBadIcon from "@material-ui/icons/MoodBad"
+import LinearProgress from "@material-ui/core/LinearProgress"
+import CheckCircleIcon from "@material-ui/icons/CheckCircle"
+import AddToPhotosIcon from "@material-ui/icons/AddToPhotos"
 
 const apiUrl = "http://13.245.17.125/api"
 export default class Home extends React.Component {
@@ -64,6 +67,9 @@ export default class Home extends React.Component {
         this.setState({ ...this.state, isFetching: false })
       })
   }
+  resetTweets = () => {
+    this.setState({ ...this.state, completedTweets: 0 })
+  }
   render() {
     return (
       <div className="tweets">
@@ -74,50 +80,72 @@ export default class Home extends React.Component {
           <h1>
             Tweets
             <span className="progress-count">
-              {this.state.completedTweets} / 1000
+              {this.state.completedTweets} / 10
             </span>
           </h1>
+          <LinearProgress
+            variant="determinate"
+            value={(this.state.completedTweets / 10) * 100}
+          />
         </div>
         <div className="cards">
-          <div className="card">
-            {this.state.isFetching ? (
-              <div className="loader">
-                <CircularProgress />
-              </div>
-            ) : (
+          {this.state.completedTweets >= 10 ? (
+            <div className="card">
               <div className="card-info">
                 <h2>
-                  <TwitterIcon className="link-icon" />
-                  &nbsp;
-                  {this.state.currentTweet && this.state.currentTweet.text}
+                  <CheckCircleIcon className="link-icon centered" />
+                  <br></br>
+                  Thank you for completing your Tweet Review.
+                  <br></br>
+                  Click below to get more Tweets
                 </h2>
-                <hr className="separator" />
-                <div className="actions">
-                  <button
-                    onClick={() => this.submitAnswer("positive")}
-                    className="action-button good"
-                  >
-                    <SentimentVerySatisfiedIcon />
-                    <span style={{ marginLeft: "15px" }}>Positive</span>
-                  </button>
-                  <button
-                    onClick={() => this.submitAnswer("neutral")}
-                    className="action-button neutral"
-                  >
-                    <SentimentSatisfiedIcon />
-                    <span style={{ marginLeft: "15px" }}>Neutral</span>
-                  </button>
-                  <button
-                    onClick={() => this.submitAnswer("negative")}
-                    className="action-button bad"
-                  >
-                    <MoodBadIcon />
-                    <span style={{ marginLeft: "15px" }}>Negative</span>
-                  </button>
-                </div>
+                <button onClick={this.resetTweets} className="complete-button">
+                  <AddToPhotosIcon />
+                  <span style={{ marginLeft: "15px" }}>Get More Tweets</span>
+                </button>
               </div>
-            )}
-          </div>
+            </div>
+          ) : (
+            <div className="card">
+              {this.state.isFetching ? (
+                <div className="loader">
+                  <CircularProgress />
+                </div>
+              ) : (
+                <div className="card-info">
+                  <h2>
+                    <TwitterIcon className="link-icon" />
+                    &nbsp;
+                    {this.state.currentTweet && this.state.currentTweet.text}
+                  </h2>
+                  <hr className="separator" />
+                  <div className="actions">
+                    <button
+                      onClick={() => this.submitAnswer("positive")}
+                      className="action-button good"
+                    >
+                      <SentimentVerySatisfiedIcon />
+                      <span style={{ marginLeft: "15px" }}>Good</span>
+                    </button>
+                    <button
+                      onClick={() => this.submitAnswer("neutral")}
+                      className="action-button neutral"
+                    >
+                      <SentimentSatisfiedIcon />
+                      <span style={{ marginLeft: "15px" }}>Neutral</span>
+                    </button>
+                    <button
+                      onClick={() => this.submitAnswer("negative")}
+                      className="action-button bad"
+                    >
+                      <MoodBadIcon />
+                      <span style={{ marginLeft: "15px" }}>Bad</span>
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     )
